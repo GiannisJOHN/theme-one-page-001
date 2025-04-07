75ee93c14f826502536f37083349b36c939bdc36
Ioannis <51725514+GiannisJOHN@users.noreply.github.com>
2025-04-07 12:02:35 +0200

    new locales architecture

--- src/locales/locales-data-aggregator.js
+++ src/locales/locales-data-aggregator.js
@@ -5,6 +5,9 @@ import enUi from '@/locales/en-ui.json';
 import elMeta from '@/locales/el-meta.json';
 import elUi from '@/locales/el-ui.json';
 
+import deMeta from '@/locales/de-meta.json';
+import deUi from '@/locales/de-ui.json';
+
 
 
 export const localesData = {
@@ -15,5 +18,9 @@ export const localesData = {
   el: {
     meta: elMeta,
     ui: elUi
+  },
+  de: {
+    meta: deMeta,
+    ui: deUi
   },
 };
