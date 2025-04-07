ba3480970e60006cdaee4a3ce274dc9ebe1e36ab
Ioannis <51725514+GiannisJOHN@users.noreply.github.com>
2025-04-07 05:23:21 +0200

    basic i18n added

---
+++ src/locales/locales-data-aggregator.js
@@ -0,0 +1,19 @@
+import enMeta from '@/locales/en-meta.json';
+import enUi from '@/locales/en-ui.json';
+
+
+import elMeta from '@/locales/el-meta.json';
+import elUi from '@/locales/el-ui.json';
+
+
+
+export const localesData = {
+  en: {
+    meta: enMeta,
+    ui: enUi
+  },
+  el: {
+    meta: elMeta,
+    ui: elUi
+  },
+};
