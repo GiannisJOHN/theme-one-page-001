2f3141b6765d21be3603acb07a5405e61b5f8e83
Ioannis <51725514+GiannisJOHN@users.noreply.github.com>
2025-04-07 17:40:29 +0200

    links added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     i18n: {
-        locales: ['en', 'el'],
+        locales: ['en', 'el', 'de'],
         defaultLocale: 'en',
         routing: {
             prefixDefaultLocale: false
