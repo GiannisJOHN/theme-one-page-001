65a379854c84f6e179491286e458ef683f6ac279
Ioannis <51725514+GiannisJOHN@users.noreply.github.com>
2025-04-07 14:59:41 +0200

    i18n added to achievements

--- src/locales/locales-data-aggregator.js
+++ src/locales/locales-data-aggregator.js
@@ -1,26 +1,32 @@
 import enMeta from '@/locales/en-meta.json';
 import enUi from '@/locales/en-ui.json';
+import enContent from '@/locales/en-content.json';
 
 
 import elMeta from '@/locales/el-meta.json';
 import elUi from '@/locales/el-ui.json';
+import elContent from '@/locales/el-content.json';
 
 import deMeta from '@/locales/de-meta.json';
 import deUi from '@/locales/de-ui.json';
+import deContent from '@/locales/de-content.json';
 
 
 
 export const localesData = {
   en: {
     meta: enMeta,
-    ui: enUi
+    ui: enUi,
+    content: enContent
   },
   el: {
     meta: elMeta,
-    ui: elUi
+    ui: elUi,
+    content: elContent
   },
   de: {
     meta: deMeta,
-    ui: deUi
+    ui: deUi,
+    content: deContent
   },
 };
